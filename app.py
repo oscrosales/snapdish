491befd86cf169e669fee73567b8f4006389a232
Oscar Rosales <orosales2@academic.rrc.ca>
2025-02-21 13:22:37 -0600

    Set up Flask

---
+++ app.py
@@ -0,0 +1,18 @@
+"""
+Title: SnapDish Web Application
+Authors: Oscar Rosales,
+Description:
+Date Created: Feb 21, 2025
+"""
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug = True, host="0.0.0.0")
